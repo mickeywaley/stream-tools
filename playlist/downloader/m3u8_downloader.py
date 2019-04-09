6d86866a7a4737c794994ef9fefb4a8197aab2c2
Li Zhiyong <lizhiyong2000@gmail.com>
2019-04-09 09:17:06 +0800

    #add m3u8 downloader

--- playlist/downloader/m3u8_downloader.py
+++ playlist/downloader/m3u8_downloader.py
@@ -26,9 +26,9 @@ def get_m3u8_body(url):
     session.mount('http://', adapter)
     session.mount('https://', adapter)
     r = session.get(url, timeout=10)
-    return r.content
+    return r.content.decode('utf-8')
 
-def download_ts_file(ts_url_list, download_dir):
+def download_ts_file(url, ts_url_list, download_dir):
     i = 0
     for ts_url in reversed(ts_url_list):
         i += 1
@@ -40,19 +40,19 @@ def download_ts_file(ts_url_list, download_dir):
         if os.path.isfile(curr_path):
             print('[warn]: file already exist')
             continue
-        urllib.urlretrieve(ts_url, curr_path)
+        urllib.request.urlretrieve(ts_url, curr_path)
 
-def main(url, download_dir):
+def download(url, download_dir):
     host = get_host(url)
     body = get_m3u8_body(url)
     ts_url_list = get_url_list(host, body)
     print('total file count:', len(ts_url_list))
-    download_ts_file(ts_url_list, download_dir)
+    download_ts_file(url, ts_url_list, download_dir)
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) > 2:
-        main(args[1], args[2])
+        download(args[1], args[2])
     else:
         print('Fail, params error, try:')
         print('python', args[0], 'your_m3u8_url', 'your_local_dir\n')
--- playlist/downloader/m3u8_playlist_downloader.py
+++ playlist/downloader/m3u8_playlist_downloader.py
@@ -2,17 +2,15 @@ import os
 
 import requests
 
+from playlist.downloader import m3u8_downloader
+
 """
 下载M3U8文件里的所有片段
 """
 
 
-/hls/cctv10/1554770478501.ts
-
-
-
 def download(url):
-    download_path = os.getcwd() + "\download"
+    download_path = os.getcwd() + "/download"
 
     if not os.path.exists(download_path):
         os.mkdir(download_path)
@@ -23,23 +21,13 @@ def download(url):
     if file_line[0] != "#EXTM3U":
         raise BaseException(u"非M3U8的链接")
 
-    unknow = True  # 用来判断是否找到了下载的地址
     for index, line in enumerate(file_line):
         if "EXTINF" in line:
-            unknow = False
             # 拼出ts片段的URL
             pd_url = file_line[index + 1]
-            res = requests.get(pd_url)
-            c_fule_name = str(file_line[index + 1])
 
-            with open(download_path + "\\" + c_fule_name, 'ab') as f:
-                f.write(res.content)
-                f.flush()
+            m3u8_downloader.download(pd_url, download_path)
 
-        if unknow:
-            raise BaseException("未找到对应的下载链接")
-        else:
-            print("下载完成")
 
 if __name__ == '__main__':
     download("https://raw.githubusercontent.com/lizhiyong2000/stream-tools/master/resource/ahnd-playlist.m3u8")
