8a1f6c30b029b9cfc5583ef0673a058ca30f5aff
lizhiyong2000 <lizhiyong2000@gmail.com>
2019-04-11 10:48:38 +0800

    #reademe 修改indexer

--- playlist/indexer/m3u8_indexer.py
+++ playlist/indexer/m3u8_indexer.py
@@ -1,9 +1,45 @@
 import pymongo
 
-connection = pymongo.MongoClient('192.168.2.26', 32717)
-db = connection.playlist
-m3u8 = db.m3u8
-m3u8.insert({'url': "李白", "name":"", "tags": [], "source": "Python", "thumb": ""})
-print("操作完成")
 
+class M3u8Indexer:
+    def __init__(self, mongodb_url, db_name, collection_name):
+        self.mongodb_url = mongodb_url
+        self.mongo_client = pymongo.MongoClient(mongodb_url)
+        self.data_list = self.mongo_client[db_name][collection_name]
+
+    def find_all(self):
+        return self.data_list.find()
+
+    def find_by_url(self, url):
+
+        myquery = { "url": url }
+        return self.data_list.find_one(myquery)
+
+    def update_thumb(self, url, thumb):
+
+        myquery = { "url": url }
+        newvalues = { "$set": { "thumb": thumb } }
+        self.data_list.update_one(myquery, newvalues)
+
+    def save(self, url, name, tag, thumb):
+        myquery = { "url": url }
+
+        doc = {
+            "url": url,
+            "name": name,
+            "thumb": thumb
+        }
+        return self.data_list.update_one(myquery, {'$set': doc, '$addToSet': {'tags': tag}}, upsert=True)
+
+
+if __name__ == '__main__':
+    mongo_url = 'mongodb://192.168.2.26:32727/replicaSet=rs0'
+    indexer = M3u8Indexer(mongo_url, 'playlist', 'm3u8')
+
+
+    for item in indexer.find_all():
+
+        indexer.save(item['url'], 'new name', 'new tag2', 'thumb')
+
+    print(list(indexer.find_all()))
 
