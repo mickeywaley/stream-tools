6e733a76f2d3006b5ee9213350206377ab5f591b
Li Zhiyong <lizhiyong2000@gmail.com>
2019-06-08 19:54:57 +0800

    add filter test code

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -7,9 +7,38 @@ import FlashMessage from '../components/FlashMessage';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+import Filters from '../components/filter/Filters';
+import InputFilter from '../components/filter/InputFilter';
+
 import { createPlayItem, editPlayItem, fetchPlayItems } from '../actions';
 
+var allMovies = [
+    {'title':'movie 1', 'year':'2016'},
+
+    {'title':'movie 2', 'year':'2018'},
+    {'title':'movie 3', 'year':'2018'},
+    {'title':'movie 4', 'year':'2018'},
+
+    {'title':'movie 5', 'year':'2018'},
+
+    {'title':'movie 6', 'year':'2018'},
+    {'title':'movie 7', 'year':'2019'},
+
+]
+
+
 class Home extends Component {
+
+    state = {
+        filters: { title: '' },
+    }
+
+    onFilterChange = (filters) => {
+        console.log(filters)
+        this.setState({ filters });
+    }
+
+
     componentDidMount() {
         this.props.dispatch(fetchPlayItems());
     }
@@ -28,11 +57,25 @@ class Home extends Component {
     };
 
     render() {
+        const { filters } = this.state;
+        const movies = allMovies.filter(({ title }) => (
+            title.includes(filters.title)
+        ))
+
         return (
 
             <div>
                 <Header />
                 { this.props.error && <FlashMessage message={ this.props.error } /> }
+
+                <Filters onChange={this.onFilterChange}>
+                    <InputFilter filterName="title" />
+                </Filters>
+                <ul>
+                    {movies.map(({ title }) => <li key={title}>{title}</li>)}
+                </ul>
+
+
                 <div className="main-content">
                     <PlayItemsPage playitems={ this.props.playitems }
                         onCreatePlayItem={ this.onCreatePlayItem }
