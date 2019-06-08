23300cad19d001ca32e103397c962684b54a7642
Li Zhiyong <lizhiyong2000@gmail.com>
2019-06-08 13:56:36 +0800

    添加后端web接口

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+//const API_BASE_URL = 'http://localhost:5000';
+
+const API_BASE_URL = 'https://freeiptv.cn/backend';
 
 const client = axios.create({
     baseURL: API_BASE_URL,
