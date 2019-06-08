fc9369363ec83ca0cb89f8aa48fa78043dc3cecc
Li Zhiyong <lizhiyong2000@gmail.com>
2019-06-08 13:12:27 +0800

    添加后端web接口

---
+++ nginx/dist/precache-manifest.b14c4956f7711a8d7392970c328ad99a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9b1a952d4514b86ddfaef7e7342cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "962970eb2037c1aeb80a",
+    "url": "/static/css/main.1696967f.chunk.css"
+  },
+  {
+    "revision": "dd80860b7b83cfd7db23",
+    "url": "/static/js/2.fb5557b7.chunk.js"
+  },
+  {
+    "revision": "962970eb2037c1aeb80a",
+    "url": "/static/js/main.edd62a50.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pushd $(dirname "${0}") > /dev/null
+basedir=$(pwd -L)
+# Use "pwd -P" for the path without links. man bash for more info.
+popd > /dev/null
+
+echo "${basedir}"
+
+
+cd "${basedir}/frontend"
+npm run build
+
+rm -rf "${basedir}/nginx/dist"
+
+cp -r "${basedir}/frontend/build" "${basedir}/nginx/dist"
