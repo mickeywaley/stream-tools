9f25d2452f22dc2d73476d9b2bb07f6365be3106
Li Zhiyong <lizhiyong2000@gmail.com>
2019-06-09 11:45:49 +0800

    channel image

--- backend/app/channel/lizhi_channel_crawler.py
+++ backend/app/channel/lizhi_channel_crawler.py
@@ -1,3 +1,7 @@
+import os
+import urllib
+from urllib.parse import urlparse
+
 import pymongo
 
 from channel.channel_crawler import ChannelCrawler
@@ -32,18 +36,55 @@ for i in range(16, 0, -1):
 
 print(result_map)
 
-mongodb_url = 'mongodb://localhost:27017'
+mongodb_url = 'mongodb://freeiptv.cn:27070'
 mongo_client = pymongo.MongoClient(mongodb_url)
 data_list = mongo_client['freeiptv']['channels']
+
+
+
+
+def save_image(img_url, file_dir):
+    try:
+
+        url = urlparse(img_url)
+        file_name = os.path.basename(url.path)
+
+        #是否有这个路径
+        if not os.path.exists(file_dir):
+            #创建路径
+            os.makedirs(file_dir)
+            #获得图片后缀
+        #拼接图片名（包含路径）
+        file_path = os.path.join(file_dir, file_name)
+        print(file_path)
+        #下载图片，并保存到文件夹中
+        urllib.request.urlretrieve(img_url, filename=file_path)
+
+        return file_name
+
+    except IOError as e:
+        print("IOError")
+    except Exception as e:
+        print("Exception")
+
+
+
+filepath = os.path.abspath(__file__)
+
+path = os.path.join(os.path.dirname(filepath), "../../../frontend/public/images/channels")
 
 
 for name, thumb in result_map.items():
 
     print("[Index-Channel]: {}".format(name))
+
+    file_name = save_image(thumb, path)
+
+
     myquery = {"name": name}
 
     doc = {
         "name": name,
-        "thumb": thumb
+        "thumb": "/images/channels/{}".format(file_name)
     }
     data_list.update_one(myquery, {'$set': doc}, upsert=True)
